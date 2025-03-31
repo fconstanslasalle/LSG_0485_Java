cd1d3aef1e398fb28e61b032344252ec5ba0e5bd
Francesc Constans <fconstans@lasalle.es>
2025-03-31 11:58:44 +0200

    Update Main.java

--- src/11_Java_Files/11_03_LlegirFitxerBinari/Main.java
+++ src/11_Java_Files/11_03_LlegirFitxerBinari/Main.java
@@ -1,3 +1,11 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
   /**
    * Mètode inicial on arrenca el programa
