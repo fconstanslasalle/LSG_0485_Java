b7a246d729f5b80421b7c2a2331ed7e84c0f689d
Francesc Constans <fconstans@lasalle.es>
2025-03-31 11:55:33 +0200

    Update Main.java

---
+++ src/11_Java_Files/11_04_BucleInfinito/Main.java
@@ -0,0 +1,27 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    String nombreArchivo = "escritura_infinita.txt";
+
+    try (BufferedWriter escritor = new BufferedWriter(new FileWriter(nombreArchivo))) {
+
+      long contador = 0;
+
+      while (true) {
+        escritor.write("L:" + contador + "\n");
+        contador++;
+
+      }
+
+    } catch (IOException e) {
+      System.err.println("Error al escribir en el archivo: " + e.getMessage());
+    }
+
+    System.out.println("Programa finalizado."); // La importancia del prints...
+  }
+}
